4338a62e354888e759e8941c8772d4f9cc0ef738
Abirami-Pragash-ZSINT0180 <abirami.pragash@zucisystems.com>
2025-06-18 10:44:37 +0530

    initial commit

---
+++ src/test/java/Runners/TestRunner.java
@@ -0,0 +1,21 @@
+package Runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/Featurefiles/searchEmployee.feature",
+        glue = {"StepDef"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-html-report.html" ,
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        monochrome = true
+)
+
+public class TestRunner {
+}
